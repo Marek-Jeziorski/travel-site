c49f03a069b84bf2c9e83fab71cc1325d61419ea
Marek <111072844+Marek-Jeziorski@users.noreply.github.com>
2025-02-16 14:02:18 +0100

    end Section 15 revealing-on-scroll

--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -1,30 +1,60 @@
+import { throttle } from 'lodash';
+import { debounce } from 'lodash';
+
 class RevealOnScroll {
-  constructor() {
-    this.itemsToReveal = document.querySelectorAll('.feature-item');
+  constructor(elements, thresholdPercent) {
+    this.thresholdPercent = thresholdPercent;
+    this.itemsToReveal = elements;
+    this.browserHeight = window.innerHeight;
     this.hideInitialy();
+    this.scrollTrottle = throttle(this.calcCaller, 200).bind(this);
     this.events();
   }
 
   events() {
-    window.addEventListener('scroll', () => {
-      this.itemsToReveal.forEach((el) => {
+    window.addEventListener('scroll', this.scrollTrottle);
+    window.addEventListener(
+      'resize',
+      debounce(() => {
+        console.log('Resize just run');
+        this.browserHeight = window.innerHeight;
+      }, 333)
+    );
+  }
+
+  calcCaller() {
+    console.log('Scroll function run');
+    this.itemsToReveal.forEach((el) => {
+      if (el.isRevealed == false) {
         this.calculateIfScrolledTo(el);
-      });
+      }
     });
   }
 
   calculateIfScrolledTo(el) {
-    let scrollPercent =
-      (el.getBoundingClientRect().y / window.innerHeight) * 100;
-    console.log(scrollPercent);
+    if (window.scrollY + this.browserHeight > el.offsetTop) {
+      console.log('Element was calculated');
+      let scrollPercent =
+        (el.getBoundingClientRect().y / this.browserHeight) * 100;
+      console.log(scrollPercent);
 
-    if (scrollPercent < 75) {
-      el.classList.add('reveal-item--is-visible');
+      if (scrollPercent < this.thresholdPercent) {
+        el.classList.add('reveal-item--is-visible');
+        el.isRevealed = true;
+        if (el.isLastItem) {
+          window.removeEventListener('scroll', this.scrollTrottle);
+        }
+      }
     }
   }
 
   hideInitialy() {
-    this.itemsToReveal.forEach((el) => el.classList.add('reveal-item'));
+    this.itemsToReveal.forEach((el) => {
+      el.classList.add('reveal-item');
+      el.isRevealed = false;
+    });
+
+    this.itemsToReveal[this.itemsToReveal.length - 1].isLastItem = true;
   }
 }
 
