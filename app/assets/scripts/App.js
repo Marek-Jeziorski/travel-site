09c5f5eb8555ec941602764c6947a3e52da4a732
Marek <111072844+Marek-Jeziorski@users.noreply.github.com>
2025-03-15 15:09:55 +0100

    END Section 17 - Building Our Own Modal

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,14 +1,16 @@
 import '../styles/styles.css';
-
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
+new Modal();
 let stickyHeader = new StickyHeader();
-let mobileMenu = new MobileMenu();
 
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 75);
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 60);
+
+let mobileMenu = new MobileMenu();
 
 // ----------------------------  -----------------------------
 
